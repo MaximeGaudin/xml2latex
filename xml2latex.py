fccba8684e26bbc58ac86d904b70f081290b2e1f
Maxime Gaudin <gaudin.maxime@gmail.com>
2011-03-22 14:44:43 +0100

    Remise au propre et ajout de la fonctionnalité d'export du préambule

--- xml2latex.py
+++ xml2latex.py
@@ -22,8 +22,11 @@ import sys
 from xml.dom import minidom
 
 indentLevel = 0
-environments = Set([])
-commands = Set([])
+environments = set([])
+commands = set([])
+
+preambule = "% Exported Preambule\n"
+code = "% Exported code\n"
 
 def getIndents():
 	global indentLevel
@@ -38,17 +41,19 @@ def explore(xmlInput):
 def handleEnvironment(N, isOpening):
 	global indentLevel
 	global environments
-
+	global code
+	
 	if isOpening:
-		print getIndents() + "\\begin{" + N.tagName + "}"
+		code += getIndents() + "\\begin{" + N.tagName + "}\n"
 		indentLevel += 1
 		environments.add(N.tagName)
 	else :
 		indentLevel -= 1	
-		print getIndents() + "\\end{" + N.tagName + "}"
+		code += getIndents() + "\\end{" + N.tagName + "}\n"
 	
 def handleCommand(N, isOpening):
 	global commands
+	global code
 
 	if(isOpening):
 		output = getIndents() + "\\" + N.tagName
@@ -57,12 +62,17 @@ def handleCommand(N, isOpening):
 		for k in N.attributes.keys(): output += "{" + N.attributes[k].value + "}" 	# Handle attributes
 		if len(N.childNodes) == 1: output += "{" + N.childNodes[0].data + "}"		# Handle datas
 
-		print output
+		code += output + "\n"
 
 def handleNode(N, isOpening):
 	if N.nodeType == N.ELEMENT_NODE:
 		if len(N.childNodes) == 0 or len(N.childNodes) == 1 and N.childNodes[0].nodeType == N.TEXT_NODE: handleCommand(N, isOpening)
 		else: handleEnvironment(N, isOpening)
+
+def generatePreambule():
+	global preambule
+	for e in environments: preambule += "\\newenvironment{" + e + "}\n{\n}\n{\n}\n\n"
+	for c in commands : preambule += "\\newcommand{" + c + "}[]\n{\n}\n\n"
 
 def printUsage():
 	print "**** XML to LaTeX converter              ****"
@@ -72,5 +82,17 @@ if len(sys.argv) == 1: printUsage()
 else:
 	try: 
 		xmlFile = minidom.parse(sys.argv[1])
-		explore(xmlFile)
 	except: sys.stderr.write("** Bad formed xml file. Parse aborted.")
+
+	try:
+		explore(xmlFile)
+		generatePreambule()
+	except: sys.stderr.write("** Oups ! Error")
+
+
+	if len(sys.argv) == 3: 
+		if sys.argv[2] == "-p": print preambule
+		if sys.argv[2] == "-c": print code 
+	else:
+		print preambule
+		print code
